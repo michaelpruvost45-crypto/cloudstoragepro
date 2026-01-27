6b7c67f663761767a8fd12dd22325001e0cad620
michaelpruvost45-crypto <michaelpruvost45@gmail.com>
2026-01-27 13:40:13 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-"scripts": {
-  "dev": "vite",
-  "build": "vite build",
-  "preview": "vite preview"
-}
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+});
