cba5beae38f4d4c79afa5b0801bd67bd453d27e7
michaelpruvost45-crypto <michaelpruvost45@gmail.com>
2026-01-30 18:51:20 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,8 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.error("❌ Variables Supabase manquantes (VITE_SUPABASE_URL / VITE_SUPABASE_ANON_KEY)");
-}
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
